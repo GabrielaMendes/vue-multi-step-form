3665f23e9711732498f911b8ff51d2da225187e4
Gabriela <gdemelo.mendes@gmail.com>
2023-05-17 18:09:23 -0300

    feature: created basic background layout

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,10 @@
 </script>
 
 <template>
-  <div></div>
+  <div class="px-4">
+    <div class="mx-auto mt-[10svh] h-[80svh] max-w-[1200px] rounded-xl bg-white">
+
+    </div>
+  </div>
 </template>
 
