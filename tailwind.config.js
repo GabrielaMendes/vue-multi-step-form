fd80ef4adb26faced133045dd5e0069e54542937
Gabriela <gdemelo.mendes@gmail.com>
2023-05-17 17:37:56 -0300

    config: added custom colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "cool-gray": "#7A7E94",
+        "light-gray": "#D6D9E6",
+        "magnolia": "#F0F6FF",
+        "alabaster": "#FAFBFF",
+      },
+    },
   },
   plugins: [],
 };
