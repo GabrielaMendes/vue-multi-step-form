c801b892cc15f23530d5d75083eb640809a95a00
Gabriela <gdemelo.mendes@gmail.com>
2023-05-17 18:29:57 -0300

    config: added vee validate to project

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import VeeValidatePlugin from "./plugins/validation";
 
 const app = createApp(App);
 
 app.use(createPinia());
+app.use(VeeValidatePlugin);
 
 app.mount("#app");
