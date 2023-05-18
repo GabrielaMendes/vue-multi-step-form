e3206443485893bd864110babcd414470da5e5ab
Gabriela <gdemelo.mendes@gmail.com>
2023-05-18 17:31:09 -0300

    config: installing and defining rules for validation

--- src/plugins/validation.js
+++ src/plugins/validation.js
@@ -1,8 +1,55 @@
-import { Form as VeeForm, Field as VeeField} from "vee-validate"
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+  configure,
+} from "vee-validate";
+import {
+  required,
+  regex,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+} from "@vee-validate/rules";
+
 
 export default {
   install(app) {
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
+    app.component("ErrorMessage", ErrorMessage);
+
+    defineRule("required", required);
+    defineRule("min", min);
+    defineRule("max", max);
+    defineRule("alpha_spaces", alphaSpaces);
+    defineRule("email", email);
+    defineRule("regex", regex);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `This field is required.`,
+          min: `This field is too short.`,
+          max: `This field is too long.`,
+          alpha_spaces: `This field may only contain alphabetical characters and spaces.`,
+          email: `You must enter a valid email.`,
+        };
+
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is invalid.`;
+
+        return message;
+      },
+
+      // default values
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 }
