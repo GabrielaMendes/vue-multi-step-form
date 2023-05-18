116c69d765f4a4f6b26db6a6fa277d0e6ed09be0
Gabriela <gdemelo.mendes@gmail.com>
2023-05-18 16:52:51 -0300

    featured: added basic layout to app

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,12 @@
 
 <template>
   <div class="px-4">
-    <div class="mx-auto mt-[10svh] h-[80svh] max-w-[1200px] rounded-xl bg-white">
+    <div class="mx-auto mt-[10svh] h-[80svh] max-w-[1200px] p-4 rounded-xl bg-white grid grid-flow-col gird-cols-3 gap-4">
 
+      <div class="h-full"></div>
+      
+      <main class="h-full">
+      </main>
     </div>
   </div>
 </template>
