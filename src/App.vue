26586c00f186c619c3e8f1dff4c5ad4473bdc0ae
Gabriela <gdemelo.mendes@gmail.com>
2023-05-19 21:29:17 -0300

    feature: created 'select your pan' step of the form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,17 @@
 <script setup>
 import PersonalInfo from "./components/PersonalInfo.vue"
+import SelectPlan from "./components/SelectPlan.vue";
 </script>
 
 <template>
   <div class="px-4">
-    <div class="mx-auto mt-[10svh] h-[80svh] max-w-[1200px] p-4 rounded-xl bg-white grid grid-flow-col gird-cols-3 gap-4">
+    <div class="mx-auto mt-[12svh] h-[76svh] max-w-[1200px] p-4 rounded-xl bg-white grid grid-flow-col gird-cols-3 gap-4">
 
       <div class="h-full"></div>
       
       <main class="h-full">
         <VeeForm class="h-full">
-          <PersonalInfo />
+          <SelectPlan />
         </VeeForm>
       </main>
     </div>
---
+++ src/components/SelectPlan.vue
@@ -0,0 +1,197 @@
+<script setup>
+import { ref } from "vue";
+import BaseFormStep from "./BaseFormStep.vue";
+
+const isMonthly = ref(true);
+const planSelected = ref("arcade");
+
+const plans = {
+  arcade: {
+    mo: 9,
+    yr: 90,
+  },
+  advanced: {
+    mo: 12,
+    yr: 120,
+  },
+  pro: {
+    mo: 15,
+    yr: 150,
+  },
+};
+</script>
+
+<template>
+  <BaseFormStep>
+    <template v-slot:title>Select Your Plan</template>
+    <template v-slot:description>
+      You have the option of monthly or yearly billing.
+    </template>
+
+    <template v-slot:main>
+      <div class="grid grid-cols-3 gap-4">
+        <!-- Arcade -->
+        <label
+          tabindex="0"
+          for="arcade"
+          @keypress.enter="planSelected = 'arcade'"
+          class="plan-option"
+          :class="
+            planSelected === 'arcade'
+              ? ' bg-alabaster border-purplish-blue'
+              : ' border-light-gray'
+          "
+        >
+          <VeeField
+            type="radio"
+            name="plan"
+            id="arcade"
+            value="arcade"
+            class="hidden"
+            v-model="planSelected"
+          />
+          <img
+            src="src/assets/images/icon-arcade.svg"
+            alt="arcade icon"
+            class="w-11 mt-2"
+          />
+          <div class="mt-9">
+            <h3 class="font-medium text-marine-blue mt-auto">Arcade</h3>
+            <p>{{ isMonthly ? `$${plans.arcade.mo}/mo` : `$${plans.arcade.yr}/yr`}}</p>
+            <transition name="slide">
+              <p v-show="!isMonthly" class="text-sm text-marine-blue">
+                2 months free
+              </p>
+            </transition>
+          </div>
+        </label>
+        <!-- Advanced -->
+        <label
+          tabindex="0"
+          for="advanced"
+          @keypress.enter="planSelected = 'advanced'"
+          class="plan-option"
+          :class="
+            planSelected === 'advanced'
+              ? ' bg-alabaster border-purplish-blue'
+              : ' border-light-gray'
+          "
+        >
+          <VeeField
+            type="radio"
+            name="plan"
+            id="advanced"
+            value="advanced"
+            class="hidden"
+            v-model="planSelected"
+          />
+          <img
+            src="src/assets/images/icon-advanced.svg"
+            alt="advanced icon"
+            class="w-11 mt-2"
+          />
+          <div class="mt-9">
+            <h3 class="font-medium text-marine-blue">Advanced</h3>
+            <p>{{ isMonthly ? `$${plans.advanced.mo}/mo` : `$${plans.advanced.yr}/yr`}}</p>
+            <transition name="slide">
+              <p v-show="!isMonthly" class="text-sm text-marine-blue">
+                2 months free
+              </p>
+            </transition>
+          </div>
+        </label>
+        <!-- Pro -->
+        <label
+          tabindex="0"
+          for="pro"
+          @keypress.enter="planSelected = 'pro'"
+          class="plan-option"
+          :class="
+            planSelected === 'pro'
+              ? ' bg-alabaster border-purplish-blue'
+              : ' border-light-gray'
+          "
+        >
+          <VeeField
+            type="radio"
+            name="plan"
+            id="pro"
+            value="pro"
+            class="hidden"
+            v-model="planSelected"
+          />
+          <img
+            src="src/assets/images/icon-pro.svg"
+            alt="pro icon"
+            class="w-12 mt-2"
+          />
+          <div class="mt-9">
+            <h3 class="font-medium text-marine-blue">Pro</h3>
+            <p>{{ isMonthly ? `$${plans.pro.mo}/mo` : `$${plans.pro.yr}/yr`}}</p>
+            <transition name="slide">
+              <p v-show="!isMonthly" class="text-sm text-marine-blue">
+                2 months free
+              </p>
+            </transition>
+          </div>
+        </label>
+      </div>
+
+      <!-- Plan Type -->
+      <div
+        class="mt-8 p-4 flex justify-center gap-6 w-full bg-alabaster rounded-lg font-medium"
+      >
+        <h3
+          class="font-medium"
+          :class="isMonthly ? ' text-marine-blue' : ' text-cool-gray'"
+        >
+          Monthly
+        </h3>
+        <label
+          tabindex="0"
+          @keypress.enter="isMonthly = !isMonthly"
+          for="checkbox"
+          class="relative h-6 w-12 rounded-full bg-marine-blue cursor-pointer after:h-4 after:w-4 focus:outline-none focus-visible:ring-offset-2 focus-visible:ring-2  focus-visible:ring-marine-blue after:rounded-full after:bg-alabaster after:absolute after:top-1 after:left-1 after:transition after:duration-300"
+          :class="{ 'after:translate-x-[150%]': !isMonthly }"
+        >
+          <input
+            type="checkbox"
+            id="checkbox"
+            v-model="isMonthly"
+            class="hidden"
+          />
+        </label>
+        <h3
+          class="font-medium"
+          :class="!isMonthly ? ' text-marine-blue' : ' text-cool-gray'"
+        >
+          Yearly
+        </h3>
+      </div>
+    </template>
+  </BaseFormStep>
+</template>
+
+<style scoped>
+.plan-option {
+  @apply flex-1 flex flex-col justify-between p-4 rounded-lg border transition duration-[400ms] cursor-pointer hover:border-purplish-blue focus:outline-none focus-visible:border-purplish-blue focus-visible:ring-offset-2 focus-visible:ring-2  focus-visible:ring-marine-blue;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transform-origin: top;
+  transition: all 0.4s ease-in-out;
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  transform: scaleY(1);
+  opacity: 1;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: scaleY(0);
+  opacity: 0;
+}
+</style>
