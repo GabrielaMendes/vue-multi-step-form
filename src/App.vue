1f95c4ff174daed77e7d35d859c03be074933cb7
Gabriela <gdemelo.mendes@gmail.com>
2023-05-24 14:37:21 -0300

    feature: made steps of the form dynamic

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,52 @@
 <script setup>
+import { computed, ref } from "vue";
 import FinishingUp from "./components/FinishingUp.vue";
 import PersonalInfo from "./components/PersonalInfo.vue";
 import PickAddOns from "./components/PickAddOns.vue";
 import SelectPlan from "./components/SelectPlan.vue";
+
+const formActive = ref(true);
+
+const currentStep = ref(1);
+
+const nextStep = () => {
+  currentStep.value += 1;
+};
+
+const prevStep = () => {
+  currentStep.value -= 1;
+};
+
+const currentStepComponent = computed(
+  () => stepList.find((step) => (step.number === currentStep.value)).component
+);
+
+const onFormSubmit = () => {
+  formActive.value = false;
+};
+
+const stepList = [
+  {
+    number: 1,
+    title: "your info",
+    component: PersonalInfo,
+  },
+  {
+    number: 2,
+    title: "select plan",
+    component: SelectPlan,
+  },
+  {
+    number: 3,
+    title: "add-ons",
+    component: PickAddOns,
+  },
+  {
+    number: 4,
+    title: "summary",
+    component: FinishingUp,
+  },
+];
 </script>
 
 <template>
@@ -10,24 +54,60 @@ import SelectPlan from "./components/SelectPlan.vue";
     <div
       class="mx-auto mt-[12svh] h-[76svh] max-w-[1200px] p-4 rounded-xl bg-white grid grid-cols-3 gap-6"
     >
-      <div class="h-full px-6 py-12 rounded-2xl  bg-[url('src/assets/images/bg-sidebar-desktop.svg')] bg-cover bg-bottom">
-        <div v-for="step in stepList" :key="step.number" class="flex items-center gap-4 mb-10">
-          <div class="w-10 h-10 border-2 border-pastel-blue rounded-full font-bold flex items-center justify-center">
-            <span class="text-alabaster">{{ step.number }}</span>
+      <!-- Side/Top Menu -->
+      <div
+        class="h-full px-6 py-12 rounded-2xl bg-[url('src/assets/images/bg-sidebar-desktop.svg')] bg-cover bg-bottom"
+      >
+        <div
+          v-for="step in stepList"
+          :key="step.number"
+          class="flex items-center gap-4 mb-10"
+        >
+          <div
+            class="w-10 h-10 border-2 border-pastel-blue rounded-full font-bold flex items-center justify-center transition-color duration-300 ease-out"
+            :class="{ 'bg-light-blue': step.number === currentStep }"
+          >
+            <span
+              class="text-alabaster"
+              :class="{ 'text-marine-blue': step.number === currentStep }"
+              >{{ step.number }}</span
+            >
           </div>
           <div>
-            <p class="uppercase text-sm text-pastel-blue">step {{ step.number }}</p>
-            <h3 class="uppercase font-bold tracking-widest text-alabaster">{{ step.title }}</h3>
+            <p class="uppercase text-sm text-pastel-blue">
+              step {{ step.number }}
+            </p>
+            <h3 class="uppercase font-bold tracking-widest text-alabaster">
+              {{ step.title }}
+            </h3>
           </div>
         </div>
       </div>
 
+      <!-- Main Form -->
       <main class="col-span-2 h-full">
-        <!-- <VeeForm class="h-full">
-        </VeeForm> -->
+        <!-- Form -->
+        <VeeForm
+          @submit="onFormSubmit"
+          v-slot="{ validate}"
+          class="h-full"
+          v-if="formActive"
+        >
+          <component
+            :is="currentStepComponent"
+            :isFirst="currentStep === 1"
+            :isLast="currentStep === stepList.length"
+            :validate="validate"
+            @go-back="prevStep"
+            @go-next="nextStep"
+          ></component>
+        </VeeForm>
 
         <!-- Thank you -->
-        <div class="h-full flex flex-col items-center justify-center text-center px-24">
+        <div
+          v-else
+          class="h-full flex flex-col items-center justify-center text-center px-24"
+        >
           <img src="src/assets/images/icon-thank-you.svg" alt="success" />
           <h1 class="font-bold text-[34px] text-marine-blue mt-6 mb-4">
             Thank you!
--- src/components/BaseFormStep.vue
+++ src/components/BaseFormStep.vue
@@ -1,25 +1,48 @@
+<script setup>
+const props = defineProps(["isFirst", "isLast", "validate"]);
+
+const emit = defineEmits(["goBack", "goNext"]);
+
+const goBack = () => {
+  emit("goBack");
+};
+
+const goNext = () => {
+  props.validate().then((result) => {
+    if (result.valid) {
+      emit("goNext");
+    }
+  });
+};
+</script>
+
 <template>
   <div class="h-full flex flex-col pt-10 pb-5 px-24">
     <section>
-      <h1 class="font-bold text-[34px] text-marine-blue"><slot name="title"></slot></h1>
+      <h1 class="font-bold text-[34px] text-marine-blue">
+        <slot name="title"></slot>
+      </h1>
       <p><slot name="description"></slot></p>
     </section>
-    
+
     <section class="mt-10">
       <slot name="main"></slot>
     </section>
-    
+
     <section class="flex justify-between mt-auto">
       <button
+        v-show="!isFirst"
         type="button"
+        @click="goBack"
         class="py-3 bg-white text-cool-gray rounded-lg font-medium hover:text-marine-blue focus-visible:outline-none focus-visible:text-marine-blue focus-visible:underline focus-visible:decoration-marine-blue focus-visible:decoration-2"
       >
         Go Back
       </button>
       <button
-        v-if="true"
-        type="submit"
-        class="w-32 py-3 bg-marine-blue text-white rounded-lg font-medium hover:bg-opacity-[0.85] focus-visible:outline-none focus-visible:bg-opacity-[0.85] focus-visible:ring-offset-2 focus-visible:ring-2 focus-visible:ring-marine-blue"
+        v-if="!isLast"
+        @click="goNext"
+        type="button"
+        class="w-32 py-3 ml-auto bg-marine-blue text-white rounded-lg font-medium hover:bg-opacity-[0.85] focus-visible:outline-none focus-visible:bg-opacity-[0.85] focus-visible:ring-offset-2 focus-visible:ring-2 focus-visible:ring-marine-blue"
       >
         Next Step
       </button>
